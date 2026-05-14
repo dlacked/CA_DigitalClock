31458ec439e249a44d69e3df52d3cace2e72562b
dlacked <imcy0104@gmail.com>
2026-05-14 16:16:36 +0900

    stopwatch v0

--- mycode.asm
+++ mycode.asm
@@ -5,25 +5,23 @@ ORG 100h
  HOUR        DW 0
  MINUTE      DW 0
  SECOND      DW 0
- ALARM_HOUR  DW ?
- ALARM_MIN   DW ?
- ALARM_SEC   DW ?
 
  msg         DB  'Enter the alarm hour (0-23): ', 0
  msg2        DB  'Enter the alarm minute (0-59): ', 0
  msg3        DB  'Enter the alarm seconds (0-59): ', 0
  alarm_msg   DB  'Alarm!', 0
  newline     DB  13,10,0
- menu_title  DB  '====================', 13,10, 0
- menu_text   DB  '     Main Menu', 13,10, 0
- menu_options DB ' 1. Alarm', 13,10, ' 2. Exit', 13,10, 0
- choose_msg DB  'Select option [1-2]: ', 0
+ equals  DB  '================================================================================', 13,10, 0
+ menu_text   DB  '                                    Main Menu', 13,10, 0
+ menu_options DB ' 1. Stop Watch', 13,10, ' 2. Exit', 13,10, 0
+ choose_msg DB  'Select option [1-2]: ', 0  
+ stopwatch_text DB 'stop watch', 0
 
 clear_screen:
     mov ah, 06h
     mov al, 0
     mov bh, 07h
-    mov cx, 0
+    mov cx, 0400h
     mov dx, 184Fh
     int 10h
     ret
@@ -34,12 +32,15 @@ clear_screen:
     MOV DS, AX
 
     ; Show main menu and wait for selection
-    LEA SI, menu_title
+    LEA SI, equals
     CALL PRINT_STRING
     LEA SI, menu_text
+    CALL PRINT_STRING    
+    LEA SI, equals
     CALL PRINT_STRING
     LEA SI, menu_options
-    CALL PRINT_STRING
+    CALL PRINT_STRING  
+    
     LEA SI, choose_msg
     CALL PRINT_STRING
 
@@ -65,63 +66,48 @@ exit_program:
     MOV SECOND, 0
 
 alarm_main:
-    CALL clear_screen
-    ; 커서를 첫 번째 줄 맨 앞(0,0)으로 이동
+    CALL clear_screen                               
+    GOTOXY 36, 2
+    LEA SI, stopwatch_text
+    CALL PRINT_STRING
     mov ah, 02h
     mov bh, 0
     mov dh, 0
     mov dl, 0
     int 10h
-
-    ; Prompt user to set alarm
-    LEA SI, msg
-    CALL PRINT_STRING 
-    CALL SCAN_NUM
-    MOV ALARM_HOUR, CX
-
-    LEA SI, msg2
-    CALL PRINT_STRING
-    CALL SCAN_NUM
-    MOV ALARM_MIN, CX
-
-    LEA SI, msg3
-    CALL PRINT_STRING
-    CALL SCAN_NUM
-    MOV ALARM_SEC, CX
-    
     
 usec:
     ; print hour
-    GOTOXY 2, 21
+    GOTOXY 36, 5
     MOV AX, HOUR
     CALL PRINT_NUM
 
     ; symbol
-    GOTOXY 5, 21
+    GOTOXY 38, 5
     mov dl,':'
     mov ah,2
     int 21h
 
     ; print minute
-    GOTOXY 8, 21
+    GOTOXY 40, 5
     MOV AX, MINUTE
     CALL PRINT_NUM
 
     ; symbol
-    GOTOXY 11, 21
+    GOTOXY 42, 5
     mov dl,':'
     mov ah,2
     int 21h
 
     ; print seconds
-    GOTOXY 14, 21
+    GOTOXY 44, 5
     MOV AX, SECOND
     CALL PRINT_NUM
 
     ; Check if alarm is set and if current time matches alarm time 
-    CALL check_alarm
+    CALL not_alarm_time
  
-mainc:
+mainc:      
  
     ; Delay for 1 second
     MOV     CX, 0FH
@@ -191,33 +177,6 @@ increment_time:
     MOV HOUR, 0
 
     JMP usec
-
-check_alarm:
-
-    ; Check if the hour matches with the alarm hour
-    MOV AX, ALARM_HOUR
-    CMP AX, HOUR
-    JNE not_alarm_time
-    
-    ; Check if the minute matches with the alarm minute
-    MOV AX,ALARM_MIN
-    CMP AX, MINUTE
-    JNE not_alarm_time
-    
-    ; Check if the second matches with the alarm second 
-    MOV AX, ALARM_SEC
-    CMP AX, SECOND
-    JNE not_alarm_time
-
-
-
-    ; Print alarm message in a new window to visually warn the user 
-    mov al, 0h
-	mov ah, 0
-	int 10h
-    LEA SI, alarm_msg
-    CALL PRINT_STRING
-
 
 not_alarm_time:
     ; Continue by jumping to the increment if the time doesn't match with the alarm
